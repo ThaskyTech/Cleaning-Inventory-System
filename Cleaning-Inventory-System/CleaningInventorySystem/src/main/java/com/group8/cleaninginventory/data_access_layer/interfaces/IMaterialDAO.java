cefad4aa9d51f4c5c2ca68a8c273a6efa32a0ede
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:27:55 +0200

    Update IMaterialDAO.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/interfaces/IMaterialDAO.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/interfaces/IMaterialDAO.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.dal.interfaces;
+package com.group8.cleaninginventory.data_access_layer.interfaces;
 
 // Defines the data access contract for Material CRUD operations (demonstrates Abstraction).
 public interface IMaterialDAO {
