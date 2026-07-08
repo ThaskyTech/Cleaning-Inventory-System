7f4904e025ba4c59d7b4a12464bef09c85390d16
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:26:41 +0200

    Update StockIssuanceService.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/StockIssuanceService.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/StockIssuanceService.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.bll;
+package com.group8.cleaninginventory.business_logic_layer;
 
 // Contains business rules for issuing stock, including stock deduction and over-issue prevention.
 public class StockIssuanceService {
