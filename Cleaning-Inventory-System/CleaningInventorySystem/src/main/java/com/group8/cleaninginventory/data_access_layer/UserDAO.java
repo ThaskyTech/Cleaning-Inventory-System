b650baf1ba49bee7511d4f6d5cc4a389eac2a208
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:30:48 +0200

    Update UserDAO.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/UserDAO.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/UserDAO.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.dal;
+package com.group8.cleaninginventory.data_access_layer;
 
 // Concrete implementation of IUserDAO; performs JDBC/SQL operations for user accounts.
 public class UserDAO {
