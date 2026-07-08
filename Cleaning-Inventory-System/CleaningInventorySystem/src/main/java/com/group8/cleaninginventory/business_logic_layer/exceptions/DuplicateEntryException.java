7e1d807a283f7b12ea21cb690370a3b8751ae56b
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:31:27 +0200

    Update DuplicateEntryException.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/exceptions/DuplicateEntryException.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/exceptions/DuplicateEntryException.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.bll.exceptions;
+package com.group8.cleaninginventory.business_logic_layer.exceptions;
 
 // Custom exception thrown when a duplicate username, email, or other unique field is detected.
 public class DuplicateEntryException extends Exception {
