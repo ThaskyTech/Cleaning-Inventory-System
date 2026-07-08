86022c7d5fd33c569654143d37c88bc659dce5eb
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:31:59 +0200

    Update ValidationException.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/exceptions/ValidationException.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/exceptions/ValidationException.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.bll.exceptions;
+package com.group8.cleaninginventory.business_logic_layer.exceptions;
 
 // Custom exception thrown when input data fails a business validation rule.
 public class ValidationException extends Exception {
