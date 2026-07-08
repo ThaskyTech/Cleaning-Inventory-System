e3ba5d4ea4aa30b6595354a4d1819712585acace
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:26:15 +0200

    Update ReportService.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/ReportService.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/ReportService.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.bll;
+package com.group8.cleaninginventory.business_logic_layer;
 
 // Aggregates data from other services/DAOs to produce dashboard statistics and reports.
 public class ReportService {
