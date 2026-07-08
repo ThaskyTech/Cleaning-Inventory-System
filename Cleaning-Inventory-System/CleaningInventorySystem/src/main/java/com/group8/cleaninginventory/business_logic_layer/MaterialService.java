259834198e37de7d657f9c85bb4af78aa9d4b152
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:25:57 +0200

    Update MaterialService.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/MaterialService.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/MaterialService.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.bll;
+package com.group8.cleaninginventory.business_logic_layer;
 
 // Contains business rules for materials, including validation, search and low-stock checks.
 public class MaterialService {
