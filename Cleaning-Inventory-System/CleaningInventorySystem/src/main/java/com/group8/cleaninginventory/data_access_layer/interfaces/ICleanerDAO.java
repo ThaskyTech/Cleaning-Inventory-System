5502d7fe4c9690d3ff33be00e98492a23a86d7a4
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:27:39 +0200

    Update ICleanerDAO.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/interfaces/ICleanerDAO.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/interfaces/ICleanerDAO.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.dal.interfaces;
+package com.group8.cleaninginventory.data_access_layer.interfaces;
 
 // Defines the data access contract for Cleaner CRUD operations.
 public interface ICleanerDAO {
