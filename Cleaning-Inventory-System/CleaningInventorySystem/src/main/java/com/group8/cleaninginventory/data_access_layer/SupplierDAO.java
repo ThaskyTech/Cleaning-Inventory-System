6274e67a3ec2d2e5d816424003f9bb34758c154b
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:30:30 +0200

    Update SupplierDAO.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/SupplierDAO.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/SupplierDAO.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.dal;
+package com.group8.cleaninginventory.data_access_layer;
 
 // Concrete implementation of ISupplierDAO; performs JDBC/SQL operations for suppliers.
 public class SupplierDAO {
