7b44578382fbb41b368c5b8a113b5d46c2d7052d
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:29:28 +0200

    Update DatabaseConnection.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/DatabaseConnection.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/DatabaseConnection.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.dal;
+package com.group8.cleaninginventory.data_access_layer;
 
 // Singleton class responsible for opening and providing the shared JDBC database connection.
 public class DatabaseConnection {
