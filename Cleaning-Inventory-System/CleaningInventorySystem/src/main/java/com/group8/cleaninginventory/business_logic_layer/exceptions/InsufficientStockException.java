a857766c8e548782104a775177af70dcb6a5b5c5
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:31:40 +0200

    Update InsufficientStockException.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/exceptions/InsufficientStockException.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/exceptions/InsufficientStockException.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.bll.exceptions;
+package com.group8.cleaninginventory.business_logic_layer.exceptions;
 
 // Custom exception thrown when a stock issuance would exceed the available material quantity.
 public class InsufficientStockException extends Exception {
