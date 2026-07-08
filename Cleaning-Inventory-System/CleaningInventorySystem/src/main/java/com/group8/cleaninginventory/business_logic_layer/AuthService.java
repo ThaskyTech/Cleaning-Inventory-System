7bf3562d7702800b553a9bee763827d46856975a
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:25:15 +0200

    Update AuthService.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/AuthService.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/AuthService.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.bll;
+package com.group8.cleaninginventory.business_logic_layer;
 
 // Handles login, registration, password validation and role-based access logic.
 public class AuthService {
