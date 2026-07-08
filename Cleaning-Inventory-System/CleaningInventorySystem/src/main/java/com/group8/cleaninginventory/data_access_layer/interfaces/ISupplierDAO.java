b820f77f6b6b17356fc3ed156f20268dc93450f4
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:28:31 +0200

    Update ISupplierDAO.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/interfaces/ISupplierDAO.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/interfaces/ISupplierDAO.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.dal.interfaces;
+package com.group8.cleaninginventory.data_access_layer.interfaces;
 
 // Defines the data access contract for Supplier CRUD operations.
 public interface ISupplierDAO {
