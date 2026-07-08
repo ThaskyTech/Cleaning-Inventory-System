f92cd63bb26cc632dddea0771b772233bbe2751b
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:27:05 +0200

    Update SupplierService.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/SupplierService.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/business_logic_layer/SupplierService.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.bll;
+package com.group8.cleaninginventory.business_logic_layer;
 
 // Contains business rules for managing suppliers.
 public class SupplierService {
