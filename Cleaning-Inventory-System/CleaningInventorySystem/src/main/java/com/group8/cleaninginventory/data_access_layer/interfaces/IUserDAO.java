4d1b7467368d0cb35f57eab4525014ab1207da45
ThaskyTech <LTMolise@outlook.com>
2026-07-08 22:28:49 +0200

    Update IUserDAO.java

--- Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/interfaces/IUserDAO.java
+++ Cleaning-Inventory-System/CleaningInventorySystem/src/main/java/com/group8/cleaninginventory/data_access_layer/interfaces/IUserDAO.java
@@ -1,4 +1,4 @@
-package com.group8.cleaninginventory.dal.interfaces;
+package com.group8.cleaninginventory.data_access_layer.interfaces;
 
 // Defines the data access contract for user accounts (login lookup, registration, duplicate checks).
 public interface IUserDAO {
