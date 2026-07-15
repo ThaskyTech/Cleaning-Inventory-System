40dc7d25e9727ebc5c7fd13a009604e0e6aa72b9
ThaskyTech <LTMolise@outlook.com>
2026-07-15 15:48:57 +0200

    Update Material.java

--- CleanPro_Desktop_App/src/cleanpro/desktopapp/model/Material.java
+++ CleanPro_Desktop_App/src/cleanpro/desktopapp/model/Material.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package cleanpro.desktopapp.model;
+package com.group8.cleaninginventory.model;
 
-/**
- *
- * @author User
- */
+// Represents a cleaning material/stock item, including quantity available and reorder level.
 public class Material {
-    
+
+    private int materialID;
+    private String materialName;
+    private String description;
+    //Remove private String category;
+    private int quantityAvailable;
+    private int reorderLevel;
+    private int supplierID;
+
+    //Unit * unit price = overall price;
+
+    public Material(int materialID, String name, String description, String category, int quantityAvailable,
+                    int reorderLevel, int supplierID) {
+        this.materialID = materialID;
+        this.materialName = name;
+        this.description = description;
+        setquantityAvailable(quantityAvailable);
+        this.reorderLevel = reorderLevel;
+        this.supplierID = supplierID;
+    }
+
+    //Getters and Setters
+
+    public int getMaterialID(){return materialID;}
+
+    public String getMaterialName(){return materialName;}
+    public void setMaterialName(String name){this.materialName = name;}
+
+    public String getDescription(){return description;}
+    public void setDescription(String description){this.description = description;}
+
+    public int getQuantityAvailable(){return quantityAvailable;}
+    public void setquantityAvailable(int quantityAvailable){this.quantityAvailable = quantityAvailable;}
+
+    public int getReorderLevel(){return reorderLevel;}
+    public void setReorderLevel(int reorderLevel){this.reorderLevel = reorderLevel;}
+
+    public int getSupplierId() { return supplierID; }
+    public void setSupplierId(int supplierId) { this.supplierID = supplierId; }
+
+    public boolean isLowStock(){return quantityAvailable <= reorderLevel;}
+
 }
