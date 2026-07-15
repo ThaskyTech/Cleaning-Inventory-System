7dd45ddc810d1c5c5a44b8a67fd651425259a642
ThaskyTech <LTMolise@outlook.com>
2026-07-15 15:53:16 +0200

    Create SessionManager.java

---
+++ CleanPro_Desktop_App/src/cleanpro/desktopapp/util/SessionManager.java
@@ -0,0 +1,27 @@
+package com.group8.cleaninginventory.util;
+
+import com.group8.cleaninginventory.model.User;
+
+
+public class SessionManager {
+    private static User currentUser;
+
+    private SessionManager() {
+    }
+
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static boolean isLoggedIn() {
+        return currentUser != null;
+    }
+
+    public static void logout() {
+        currentUser = null;
+    }
+}
